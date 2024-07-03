a003002c9b2894a363e70c55acf6dca219c55914
Matias-F-Diaz-C <mfc.diaz@alumno.etec.um.edu.ar>
2024-07-03 11:45:28 -0300

    Correcion de documentacion jsdoc

--- index.js
+++ index.js
@@ -36,31 +36,31 @@ app.get('/books', async (req, res) => {
     res.json(books);
 });
 /**
- * Obtiene un libro por su ID.
+ * Obtiene un libro por su ID
  * @param {express.Request} req - Objeto de solicitud a la base de datos
  * @param {express.Response} res - Objeto de respuesta a la base de datos
- * @param {number} req.params.id - ID del libro.
+ * @param {number} req.params.id - ID del libro
  */
 app.get('/books/:id', async (req, res) => {
     const book = await Book.findByPk(req.params.id);
     res.json(book);
 });
 /**
- * Crea un nuevo libro.
+ * Crea un nuevo libro
  * @param {express.Request} req - Objeto de solicitud a la base de datos
  * @param {express.Response} res - Objeto de respuesta a la base de datos
- * @param {Object} req.body - Datos del libro a crear.
+ * @param {Object} req.body - Datos del libro a crear
  */
 app.post('/books', async (req, res) => {
     const book = await Book.create(req.body);
     res.json(book);
 });
 /**
- * Actualiza un libro existente.
+ * Actualiza un libro existente
  * @param {express.Request} req - Objeto de solicitud a la base de datos
  * @param {express.Response} res - Objeto de respuesta a la base de datos
- * @param {number} req.params.id - ID del libro a actualizar.
- * @param {Object} req.body - Datos actualizados del libro.
+ * @param {number} req.params.id - ID del libro a actualizar
+ * @param {Object} req.body - Datos actualizados del libro
  */
 app.put('/books/:id', async (req, res) => {
     const book = await Book.findByPk(req.params.id);
@@ -72,10 +72,10 @@ app.put('/books/:id', async (req, res) => {
     }
 });
 /**
- * Elimina un libro por su ID.
+ * Elimina un libro por su ID
  * @param {express.Request} req - Objeto de solicitud a la base de datos
  * @param {express.Response} res - Objeto de respuesta a la base de datos
- * @param {number} req.params.id - ID del libro a eliminar.
+ * @param {number} req.params.id - ID del libro a eliminar
  */
 app.delete('/books/:id', async (req, res) => {
     const book = await Book.findByPk(req.params.id);
@@ -86,7 +86,10 @@ app.delete('/books/:id', async (req, res) => {
         res.status(404).json({ message: 'book not found' });
     }
 });
-
+/**
+ * Inicia el servidor en el puerto especificado
+ * @param {int} port - Numero de puerto
+ */
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
